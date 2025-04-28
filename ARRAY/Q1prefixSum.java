d10a69dfa29bdbc4aacbbaa02902ac2409fad796
Zubaida Sehnaz <zubaidasehnaz@gmail.com>
2025-04-28 17:11:26 +0530

    Create Q1prefixSum.java

---
+++ ARRAY/Q1prefixSum.java
@@ -0,0 +1,12 @@
+package ARRAY;
+
+//Prefix Sum Array: Find the sum of elements in a given range [L, R] using a prefix sum array.
+
+public class Q1prefixSum {
+
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
